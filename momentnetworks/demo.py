95dab52431ae1181dfd1338b6b6f0f709416f76e
Niall <niall.jeffrey.15@ucl.ac.uk>
2020-11-13 12:26:58 +0000

    added neural code to demo

--- momentnetworks/demo.py
+++ momentnetworks/demo.py
@@ -17,29 +17,73 @@ import emcee as mc
 import getdist
 
 
+
+class simple_leaky():
+    """
+    A simple MLP with LeakyReLU activation
+    """
+    
+    def __init__(self, input_size, output_size, learning_rate=None):
+        """
+        Initialisation
+        :param map_size: size of square image (there are map_size**2 pixels)
+        :param learning_rate: learning rate for the optimizer
+        """
+        self.input_size = input_size
+        self.learning_rate = learning_rate
+        self.output_size = output_size
+        
+    def model(self):
+        print(self.input_size)
+        
+        input_data = (Input(shape=(self.input_size,)))
+
+        x1 = Dense(self.input_size, input_dim=self.input_size, kernel_initializer='normal')(input_data)
+        x1 = LeakyReLU(alpha=0.1)(x1)
+        x2 = Dense(self.input_size*2, kernel_initializer='normal')(x1)
+        x2 = LeakyReLU(alpha=0.1)(x2)
+        x3 = Dense(self.input_size, kernel_initializer='normal')(x2)
+        x3 = LeakyReLU(alpha=0.1)(x3)
+        x4 = Dense(self.output_size, kernel_initializer='normal')(x3)
+        
+
+        dense_model = Model(input_data, x4)
+        dense_model.summary()
+
+        if self.learning_rate is None:
+            dense_model.compile(optimizer='adam', loss='mse')
+        else:
+            dense_model.compile(optimizer=optimizers.Adam(lr=self.learning_rate), loss='mse')
+
+        return dense_model
+
 def generate__signal(size, n_training):
-  cov_signal_fourier = np.identity(size)*np.logspace(size,size*0.9,size)/np.sum(np.logspace(size,size*0.9,size))
+    """
+    Generate high-dimensional signal with covariance
+    """
+    cov_signal_fourier = np.identity(size)*np.logspace(size,size*0.9,size)/np.sum(np.logspace(size,size*0.9,size))
 
-  training_z = np.random.multivariate_normal(np.zeros(size),cov_signal_fourier,n_training) + \
+    training_z = np.random.multivariate_normal(np.zeros(size),cov_signal_fourier,n_training) + \
                   1j*np.random.multivariate_normal(np.zeros(size), cov_signal_fourier,n_training)
-  training_z = np.fft.fft(training_z).real
+    training_z = np.fft.fft(training_z).real
 
-  cov_signal = np.identity(size)*0.
-  mean_z = np.mean(training_z,axis=0)
-  for i in range(n_training-1):
+    cov_signal = np.identity(size)*0.
+    mean_z = np.mean(training_z,axis=0)
+    for i in range(n_training-1):
     cov_signal += np.outer(training_z[i] - mean_z,training_z[i]-mean_z) /np.float(n_training-1.)
 
-  cov_signal = np.where((cov_signal<0.9)&(cov_signal>0.2),cov_signal+0.3,cov_signal)
-  training_z = np.random.multivariate_normal(np.zeros(size),cov_signal,n_training)
+    cov_signal = np.where((cov_signal<0.9)&(cov_signal>0.2),cov_signal+0.3,cov_signal)
+    training_z = np.random.multivariate_normal(np.zeros(size),cov_signal,n_training)
 
-  cov_signal = np.identity(size)*0.
-  mean_z = np.mean(training_z,axis=0)
-  for i in range(n_training-1):
+    cov_signal = np.identity(size)*0.
+    mean_z = np.mean(training_z,axis=0)
+    for i in range(n_training-1):
     cov_signal += np.outer(training_z[i] - mean_z,training_z[i]-mean_z) /np.float(n_training-1.)
 
-  cov_signal_inv = np.linalg.pinv(cov_signal)
+    cov_signal_inv = np.linalg.pinv(cov_signal)
 
-  return training_z, cov_signal, cov_signal_inv
+    return training_z, cov_signal, cov_signal_inv
+
 
 def log_prior(theta, data,cov_signal_inv):
     return -0.5*np.inner(theta,np.inner(cov_signal_inv,theta))
@@ -63,3 +107,4 @@ def initial_parameters(theta, relative_sigma):
     """
     theta = np.array(theta)
     return np.random.normal(theta, np.abs(theta * relative_sigma))
+
